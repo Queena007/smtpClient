76bd3480780e9dbeabee20d73ec5406619752e87
Queena007 <124480642+Queena007@users.noreply.github.com>
2023-03-01 08:35:28 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -1,36 +1,39 @@
 from socket import *
-
 def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n Computer networks are great!"
     endmsg = "\r\n.\r\n"
-
-
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
-
     # Fill in start
     clientSocket = socket(AF_INET,SOCK_STREAM)
+    #clientSocket.bind((mailserver, port))
     clientSocket.connect((mailserver, port))
     # Fill in end
-
     recv = clientSocket.recv(1024).decode()
+    # print('test')
+    # print(recv) #You can use these print statement to validate return codes from the server.
+    # if recv[:3] != '220':
+    #    print('220 reply not received from server.')
     # Send HELLO command and print server response.
-
     helloCommand = 'HELO Mark\r\n'
     clientSocket.sendall(helloCommand.encode())
-    # Fill in start
+    recv1 = clientSocket.recv(1024).decode()
+    # print(recv1)
+    # if recv1[:3] != '250':
+    #    print('250 reply not received from server.')
 
-    mailfromCommand = 'MAIL FROM: <mail@mail.com>\r\n'
-    clientSocket.sendall(mailfromCommand.encode())
+    # Send MAIL FROM command and handle server response.
+    # Fill in start
+    mailfromCommand = 'MAIL FROM: <mail@mail.com>\r\n' clientSocket.sendall(mailfromCommand.encode())
+    recv2 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
-   
+    # receiver = input("Send email to: ")
     receiver = 'cubicmaara@gmail.com'
-    toCommand = 'RCPT TO: <'+ receiver +'>\r\n'
-    clientSocket.sendall(toCommand.encode())
+    toCommand = 'RCPT TO: <'+ receiver +'>\r\n'    clientSocket.sendall(toCommand.encode())
+    recv3 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send DATA command and handle server response.
@@ -39,8 +42,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     dataCommand = 'DATA\r\n'
     print (dataCommand)
     clientSocket.sendall(dataCommand.encode())
+    recv4 = clientSocket.recv(1024).decode()
     # Fill in end
-
 
     # Send message data.
     # Fill in start
@@ -49,16 +52,14 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     message = "Subject: "+Subject+"\r\n\r\n"+Text
     clientSocket.sendall(message.encode())
+    recv5 = clientSocket.recv(1024).decode()
     # Fill in end
 
-
     # Message ends with a single period, send message end and handle server response.
-
     # Fill in start
     endmsg = "\r\n.\r\n"
     clientSocket.sendall(endmsg.encode())
     recv6 = clientSocket.recv(1024).decode()
-
     # Fill in end
 
     # Send QUIT command and handle server response.
@@ -67,9 +68,6 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket.sendall(quit_msg.encode())
     recv7 = clientSocket.recv(1024).decode()
     clientSocket.close()
-
     # Fill in end
-
-
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
